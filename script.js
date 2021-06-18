bb9244cfa89c85c1e0c6bcb648192e6ec23b7dbf
ANDERSON131A <ANDERSON131A@users.noreply.github.com>
2021-06-18 21:03:04 +0800

    :v

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 
 function get() {
 s = $("#s").val();//取得key
-const api = 'http://127.0.0.1:19132/s/' + s;
+const api = 'http://127.0.0.1:19132/s/' + s;//api請求地址
 document.title = s+"-搜尋結果"; //設定標題
 $.ajax({
   type: "GET",
@@ -14,13 +14,13 @@ $.ajax({
   beforeSend: function(){
     // request api.ancraft.one
   },
-  success: function (response) {
-    const jsond = [];
-    jsond.push(...response);//儲存json到列表中
-    lodata(jsond);
+  success: function (response) { //api請求成功
+    const jsond = []; //資料總表
+    jsond.push(...response); //儲存json到列表中
+    lodata(jsond); //開始處理資料
   },
   error: function (thrownError) {
-    console.log(thrownError);
+    console.log(thrownError); //列印錯誤訊息
   }
 });
 
@@ -28,11 +28,12 @@ function lodata(jsond) { //處理數據
   
   
 	// console.log(jsond);
-	var name = [], standard_deviation = []; //分類資料種
-  var price = [], by = [], img = [],sho = [],store = [];
+	var name = [], standard_deviation = []; //分類資料
+  var price = [], by = [], img = [],sho = [],store = []; //分類資料
 
-	for (var i=0; i<jsond.length; i++) {
-		row = jsond[i];const shopkeeper = row['by'].split('-');
+	for (var i=0; i<jsond.length; i++) { //循環處理每列資料
+		row = jsond[i]; //正在處理列
+    const shopkeeper = row['by'].split('-');//切割資料
 		name.push( row['name'] );
 		price.push( row['price'] );
 		store.push( shopkeeper[0] );
@@ -41,8 +42,8 @@ function lodata(jsond) { //處理數據
     img.push( row['image'] );
     // console.log(row)
 	}
-	makePlotly( name, price, by, store, sho, standard_deviation, jsond );
-  stats(name,price,by,img,store,sho,jsond);
+	makePlotly( name, price, by, store, sho, standard_deviation, jsond ); //製作圖表
+  stats(name,price,by,img,store,sho,jsond); //統計
 }
 
 function makePlotly(name, price, by, store, sho, standard_deviation, jsond ){
@@ -75,7 +76,7 @@ function makePlotly(name, price, by, store, sho, standard_deviation, jsond ){
 
 //廠商占比
   var traces = [{
-    labels: _.compact(store),
+    labels: _.compact(store), //處理錯誤或空值
     type: 'pie',
     automargin: false
   }];
@@ -101,9 +102,10 @@ Plotly.newPlot('anc', traces,
 
 };//plotend
 
+
 function stats(name,price,by,img,store,sho,jsond){
   price.forEach(function(v,i,a){
-    a[i] = v.replace(/,/g, "");
+    a[i] = v.replace(/,/g, ""); //去除逗號
   });
 //$("#data_1").text(jsond.length); //搜尋到的數量
 //$("#data_2").text(Math.max(...price));
@@ -115,7 +117,7 @@ var data = {
   "data_3":"0",
   "data_4":"0",
 };
-anime({
+anime({ //統計數字自動增加動畫
   targets: data,
   data_1: jsond.length,
   data_2: Math.max(...price),
@@ -132,12 +134,16 @@ anime({
   }
 });
 
-};//statsend
+};//statsend 統計結束
 
 
 
-};//get
+};//get ajax api請求結束
 
 
 
-
+$(document).ready(function(){
+  $("input").keyup(function(id){
+    if(id.key === "Enter"){get()};
+    });
+  });//按下enter以查詢資料
