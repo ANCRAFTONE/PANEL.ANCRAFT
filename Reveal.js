14db535786e28cb80938a1b0cff8bab27ff1b877
ANDERSON131A <ANDERSON131A@users.noreply.github.com>
2021-07-11 09:12:14 +0800

    Update Reveal.js

--- Reveal.js
+++ Reveal.js
@@ -1,4 +1,5 @@
 
 ScrollReveal().reveal('.top',{distance:'11em',origin:'top'});  //ScrollReveal
 ScrollReveal().reveal('.stats',{distance:'11em',origin:'top',interval: 60});  //ScrollReveal
+ScrollReveal().reveal('.path',{distance:'131em',origin:'top',interval: 90,delay:310});  //ScrollReveal
 ScrollReveal().reveal('.block',{distance:'11em',origin:'top',interval: 60,delay:310});  //ScrollReveal
