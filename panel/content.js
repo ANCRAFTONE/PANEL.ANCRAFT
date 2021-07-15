8b46db3f976d849bf95e66822a00a57868ec9ebd
ANDERSON131A <ANDERSON131A@users.noreply.github.com>
2021-07-15 21:50:14 +0800

    add

--- panel/content.js
+++ panel/content.js
@@ -1,3 +1,7 @@
 function content(){
-    console.log($("#data_5").text("s"));
+    $(".block:nth-child(1) h5").text("資料庫總搜尋數")
+    $(".block:nth-child(2) h5").text("本項目最高標記")
+    $(".block:nth-child(3) h5").text("本項目最高電量")
+    $(".block:nth-child(4) h5").text("本項目最低標準")
+    $(".block:nth-child(5) h5").text("數據的流動總量")
 }
