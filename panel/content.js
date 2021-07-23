e0427bf5b67560eadfa2d0e473934486c862fdab
ANDERSON131A <ANDERSON131A@users.noreply.github.com>
2021-07-23 18:13:24 +0800

    Update content.js

--- panel/content.js
+++ panel/content.js
@@ -1,8 +1,8 @@
 function content(){
-    $(".block:nth-child(1) h5").text("資料庫總搜尋數")
-    $(".block:nth-child(2) h5").text("本項目最高標記")
-    $(".block:nth-child(3) h5").text("本項目最高電量")
-    $(".block:nth-child(4) h5").text("本項目最低標準")
-    $(".block:nth-child(5) h5").text("數據的流動總量")
-    $(".titletext").text("dashboard")
+    $(".stats.block:nth-child(1) h5").text("資料庫總搜尋數")
+    $(".stats.block:nth-child(2) h5").text("本項目最高標記")
+    $(".stats.block:nth-child(3) h5").text("本項目最高電量")
+    $(".stats.block:nth-child(4) h5").text("本項目最低標準")
+    $(".stats.block:nth-child(5) h5").text("數據的流動總量")
+    $(".titletext").text("ANCPANEL")
 }
