d3786f1e7cbd2bdf1660defe3ed98ad76e6d36a7
ANDERSON131A <ANDERSONSERVER@outlook.com>
2022-01-11 00:42:00 +0800

    add App

---
+++ src/app.js
@@ -0,0 +1,16 @@
+
+
+import './App.css';
+import NavMenuBarSection from './NavMenuBarSection';
+import MainSection from './MainSection';
+
+function App() {
+  return (
+    <main id="ANCRAFTONE" class="NETANCRAFTONE" name="ANCRAFTONE">  
+      <NavMenuBarSection />
+      <MainSection />
+    </main> 
+  );
+}
+
+export default App;
