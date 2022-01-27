25a108cef425d452d0c6dcce546d62376ebabccf
ANDERSON131A <ANDERSONSERVER@outlook.com>
2022-01-27 18:50:29 +0800

    Create land.jsx

---
+++ src/pages/land.jsx
@@ -0,0 +1,11 @@
+
+
+// import './ANCRAFTONE.css';
+// import ANCRAFTONE from './ANCRAFTONE';
+import * as React from 'react';
+
+export default function land({}) {
+	return (
+		<a>landing page</a>
+	);
+}
