497d031985a3769789d23b7f7b1f3ed95ee93105
ANDERSON131A <ANDERSON131A@users.noreply.github.com>
2022-05-13 16:14:58 +0800

    ㄏ

---
+++ src/pages/account.jsx
@@ -0,0 +1,11 @@
+
+
+// import './ANCRAFTONE.css';
+// import ANCRAFTONE from './ANCRAFTONE';
+import * as React from 'react';
+
+export default function ANCRAFTONE({}) {
+	return (
+		<a>login</a>
+	);
+}
